4808b8b317e119c3e1f4abe8defc354c66bf7ed9
ntnobara <nguyentoan231196@gmail.com>
2024-11-19 21:29:23 +0700

    write function to set user and write json to gatorconfig.json file

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,12 +6,28 @@ import (
 	"path/filepath"
 )
 
+const configFileName = "gatorconfig.json"
+
 type Config struct {
-	DBURL string `json:"db_url"`
+	DbUrl    string `json:"db_url"`
+	Username string `json:"current_user_name"`
+}
+
+func (c *Config) SetUser() {
+	c.Username = "nt2311"
+
+	byteData, err := json.Marshal(c)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = os.WriteFile(configFileName, byteData, 0666); err != nil {
+		panic(err)
+	}
 }
 
 func Read() Config {
-	byteData, err := os.ReadFile(filepath.Join("gatorconfig.json"))
+	byteData, err := os.ReadFile(filepath.Join(configFileName))
 	if err != nil {
 		return Config{}
 	}
--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/nt2311-vn/Blog_Aggregator/internal/config"
+)
+
 func main() {
+	c := config.Read()
+	c.SetUser()
+
+	byteData, err := os.ReadFile("./gatorconfig.json")
+	if err != nil {
+		fmt.Printf("cannot reafile config %s", err)
+	}
+
+	fmt.Println(string(byteData))
 }
